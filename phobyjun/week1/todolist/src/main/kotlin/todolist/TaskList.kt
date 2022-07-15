f046223a55231fc2207a26e357b2084f3c4556a4
phobyjun <phobyjun@gmail.com>
2022-07-15 20:21:08 +0900

    ADD: new task with due date method

--- phobyjun/week1/todolist/src/main/kotlin/todolist/TaskList.kt
+++ phobyjun/week1/todolist/src/main/kotlin/todolist/TaskList.kt
@@ -1,6 +1,7 @@
 package todolist
 
 import todolist.todo.Task
+import java.time.LocalDate
 
 class TaskList {
     private var taskList = mutableListOf<Task>()
@@ -14,5 +15,10 @@ class TaskList {
     fun newTask(text: String) {
         val task = Task(text)
         this.taskList.add(task)
+    }
+
+    fun newTaskWithDueDate(text: String, dueDate: String) {
+        val taskWithDate: (String, String) -> Task = { t, d -> Task(t, LocalDate.parse(d)) }
+        this.taskList.add(taskWithDate(text, dueDate))
     }
 }
--- phobyjun/week1/todolist/src/main/kotlin/todolist/todo/Task.kt
+++ phobyjun/week1/todolist/src/main/kotlin/todolist/todo/Task.kt
@@ -7,6 +7,7 @@ data class Task(
     val dueDate: LocalDate?,
     val status: TaskStatus,
 ) {
-    constructor(text: String) : this(text, null, TaskStatus.READY) {
-    }
+    constructor(text: String) : this(text, null, TaskStatus.READY)
+
+    constructor(text: String, dueDate: LocalDate) : this(text, dueDate, TaskStatus.READY)
 }
