7a6148e272477197e40a0a21e3ba78f70f83da3b
phobyjun <phobyjun@gmail.com>
2022-08-05 15:06:49 +0900

    FIX: Remove password field in accountResponseDto

--- phobyjun/week4/meongDamGil/src/main/kotlin/io/dasom/meongdamgil/dto/account/AccountResponseDto.kt
+++ phobyjun/week4/meongDamGil/src/main/kotlin/io/dasom/meongdamgil/dto/account/AccountResponseDto.kt
@@ -6,13 +6,11 @@ import java.time.ZonedDateTime
 
 data class AccountResponseDto(
     val email: String,
-    val password: String,
     val userRole: UserRole?,
     val createdAt: ZonedDateTime?,
 ) {
     constructor(account: Account) : this(
         email = account.email,
-        password = account.password,
         userRole = account.userRole,
         createdAt = account.createdAt
     )
