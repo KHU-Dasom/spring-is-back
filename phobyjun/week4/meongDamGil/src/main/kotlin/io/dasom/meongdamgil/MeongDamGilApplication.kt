5eacc8d647f0368b31cb1e9e5b231b2595dc773c
phobyjun <phobyjun@gmail.com>
2022-08-05 15:39:01 +0900

    ADD: Gradle build file and spring main application class

---
+++ phobyjun/week4/meongDamGil/src/main/kotlin/io/dasom/meongdamgil/MeongDamGilApplication.kt
@@ -0,0 +1,11 @@
+package io.dasom.meongdamgil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeongDamGilApplication
+
+fun main(args: Array<String>) {
+    runApplication<MeongDamGilApplication>(*args)
+}
